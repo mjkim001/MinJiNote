897af39789e7f7d752a2bbda3a957d1c61ed9a70
MinJi Kim <dev.kimminji@gmail.com>
2022-02-04 17:31:21 +0900

    Add files via upload
    test file

---
+++ JDBCTest.java
@@ -0,0 +1,19 @@
+package k_jdbc;
+
+import java.util.*;
+
+public class JDBCTest {
+	public static void main(String[] args) {
+		String sql = "select *"
+				+ " from cart"
+				+ " where cart_member = ?"
+				+ " and cart_qty > ?";
+		ArrayList<Object> param = new ArrayList<Object>();
+		param.add("a001");
+		param.add(5);
+		
+		List<Map<String, Object>> list = JDBCUtil.selectList(sql, param);
+		
+		System.out.println(list);
+	}
+}
