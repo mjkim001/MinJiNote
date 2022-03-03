10bd0587606b615d0935172a74d9cea69c5e925b
MinJi Kim <dev.kimminji@gmail.com>
2022-03-03 15:28:47 +0900

    Add files via upload

---
+++ java/ArrayListTest.java
@@ -0,0 +1,13 @@
+package kr.or.ddit.basic;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+	public static void main(String[] args) {
+		//ArrayList는 Vector와 기본적으로 동일
+		ArrayList list1 = new ArrayList();
+		
+		list1.add("aaaa");
+		
+	}
+}
