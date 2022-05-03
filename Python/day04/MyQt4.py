4bf6343e50e17117879d56cdf3f6703bd3172cb3
MinJi Kim <dev.kimminji@gmail.com>
2022-05-03 09:28:57 +0900

    예제 코드 수정

--- Python/day04/MyQt4.py
+++ Python/day04/MyQt4.py
@@ -19,18 +19,22 @@ class MainClass(QMainWindow, form_class):
     def myclick(self):
         mine = self.le_mine.text()
         rnd = random.random()
+        com = ""
+        result = ""
         
         if rnd > 0.5:
             com = "짝"
-            self.le_com.setText(com)
         else:
             com = "홀"
-            self.le_com.setText(com)
-        
+            
+        self.le_com.setText(com)
+
         if mine == com:
-            self.le_result.setText("맞췄습니다.")
+            result = "맞췄습니다."
         else:
-            self.le_result.setText("틀렸습니다.")
+            result = "틀렸습니다."
+        
+        self.le_result.setText(result)
         
         
 if __name__ == '__main__':
--- Python/day04/MyQt6.py
+++ Python/day04/MyQt6.py
@@ -16,13 +16,20 @@ class MainClass(QMainWindow, form_class):
         
     
     def myclick(self):
-        
-        arr45 = list(range(1,45+1))
+        arr45 = list(range(1,45+1)) #list를 붙여주면 배열로 
         arr6 = []
         
         # for i in range(1,45+1):
         #     arr45.append(i)
         
+        
+        # 랜덤으로 뽑아서 0번째 자리와 자리를 바꿔 여러번 반복
+        # rnd = int(random.random()*45)
+        # a = arr45[rnd]
+        # b = arr45[0]
+        # arr45[0] = a
+        # arr45[rnd] = b
+
         random.shuffle(arr45)
         
         for i in range(6):
