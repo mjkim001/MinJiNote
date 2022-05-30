74014521fa73ab4e62d89a08dbc2b4a3ff3a3710
MinJi Kim <dev.kimminji@gmail.com>
2022-05-30 12:18:52 +0900

    회원 정보

--- Spring/command_bootstarp_1/WebContent/resources/js/common.js
+++ Spring/command_bootstarp_1/WebContent/resources/js/common.js
@@ -16,13 +16,14 @@ function CloseWindow(){
 }
 
 //사용자 사진 출력
-function MemberPictureThumb(contextPsth){
+function MemberPictureThumb(contextPath){
 	for(var target of document.querySelectorAll('.manPicture')){
-		var id =  target.getAttribute('data-id');
+		var id = target.getAttribute('data-id');
 		
-		target.style.backgroundImage = "url('"+contextPsth+"/member/getPicture.do?id="+id+"')"
+		target.style.backgroundImage = "url('" +contextPath + "/member/getPicture.do?id="+id+"')";
 		target.style.backgroundPosition = "center";
 		target.style.backgroundRepeat = "no-repeat";
 		target.style.backgroundSize = "cover";
+		
 	}
 }
